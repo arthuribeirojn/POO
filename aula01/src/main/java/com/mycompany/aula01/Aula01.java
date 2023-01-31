216e303e2cd1c05223e85a85dbe16749e990be8c
Arthur Ribeiro <110831236+arthuribeirojn@users.noreply.github.com>
2023-01-31 11:57:29 -0300

    Criei uma caneta POO

---
+++ aula01/src/main/java/com/mycompany/aula01/Aula01.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.aula01;
+
+public class Aula01 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();        
+        c1.status();
+        c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Hostnet";
+        c2.cor = "Preta";
+        c2.ponta = 0.5f;
+        c2.status();
+        c2.destampar();
+        c2.rabiscar();
+    }
+}
