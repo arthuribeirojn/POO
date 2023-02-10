fffce6769d6cc577053cd2a93ca8540489cc6c17
Arthur Ribeiro <110831236+arthuribeirojn@users.noreply.github.com>
2023-02-09 21:57:08 -0300

    Continua com lutador

--- aula07/src/main/java/com/mycompany/aula07/Aula07.java
+++ aula07/src/main/java/com/mycompany/aula07/Aula07.java
@@ -1,16 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.aula07;
 
-/**
- *
- * @author Artur
- */
+
 public class Aula07 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    int L[];
+    
+    l[0] = new Lutador("Pretty Boy","França", 31, 1.75, 68.9, 11, 3, 1 );    
+    l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+    l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+    l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 12, 0, 2);
+    l[0].apresentar();
+    l[1].apresentar();
+    l[4].apresentar();
+    l[2].apresentar();
+    l[3].apresentar();
+    
+    
     }
-}
--- aula07/src/main/java/com/mycompany/aula07/Lutador.java
+++ aula07/src/main/java/com/mycompany/aula07/Lutador.java
@@ -1,13 +1,141 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.aula07;
 
-/**
- *
- * @author Artur
- */
 public class Lutador {
-    
+ private String nome;
+ private String nacionalidade;
+ private int idade;
+ private float altura;
+ private float peso;
+ private String categoria;
+ private int vitorias;
+ private int derrotas;
+ private int empates;
+
+    public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.setPeso(pe);
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setName(String no) {
+        this.nome = no;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float al) {
+        this.altura = al;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float pe) {
+        this.peso = pe;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria(String ca) {
+        this.categoria = ca;
+        if(peso<52.2){
+            this.setCategoria("Invalido");
+        }else if(peso<= 70.3){
+            this.setCategoria("Leve");
+        }else if(peso<= 83.9){
+            this.setCategoria("Médio");
+        }else if(peso<= 120.2){
+            this.setCategoria("Pesado");
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+ 
+ public void apresentar(){
+     System.out.println("Lutador: " + getNome());
+     System.out.println("Origem: " + getNacionalidade());
+     System.out.println(getIdade() + " anos");
+     System.out.println(getAltura() + " m de altura ");
+     System.out.println("Pesando:" + getPeso());
+     System.out.println("Ganhou: " + getVitorias());
+     System.out.println("Perdeu: " + getDerrotas());
+     System.out.println("Empatou: " + getEmpates());
+ }
+ public void status(){
+     System.out.println("Lutador: " + getNome());
+     System.out.println("Pesando:" + getPeso());
+     System.out.println("Ganhou: " + getVitorias());
+     System.out.println("Perdeu: " + getDerrotas());
+     System.out.println("Empatou: " + getEmpates());
+     
+ }
+ public void ganhaLuta(){
+    this.setVitorias(getVitorias() + 1);
+     }     
+ 
+ public void perderLuta() {
+      this.setDerrotas(getDerrotas() + 1); 
+     
+ } 
+ public void empatarLuta(){
+     this.setDerrotas(getDerrotas() + 1);
+     
+ }
+
+
 }
